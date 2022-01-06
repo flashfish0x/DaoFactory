8808256600b9a6c0f3b200946ae10a8b0bcbaf1c
flashfish <flashfish0x@protonmail.com>
2022-01-06 21:34:22 +0000

    black

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,31 +2,29 @@ import pytest
 from brownie import Wei, config
 
 
-
 @pytest.fixture
-def yfi_whale(accounts,yfi):
-    #maker yfi
-    acc = accounts.at('0x3ff33d9162aD47660083D7DC4bC02Fb231c81677', force=True)
+def yfi_whale(accounts, yfi):
+    # maker yfi
+    acc = accounts.at("0x3ff33d9162aD47660083D7DC4bC02Fb231c81677", force=True)
 
     assert yfi.balanceOf(acc) > 0
 
     yield acc
 
 
-
 @pytest.fixture
 def gov(accounts):
-    #ychad.eth
-    acc = accounts.at('0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52', force=True)
+    # ychad.eth
+    acc = accounts.at("0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52", force=True)
 
     yield acc
 
 
 @pytest.fixture
 def dai(interface):
-    yield interface.ERC20('0x6b175474e89094c44da98b954eedeac495271d0f')
+    yield interface.ERC20("0x6b175474e89094c44da98b954eedeac495271d0f")
+
 
 @pytest.fixture
 def yfi(interface):
-    yield interface.ERC20('0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e')
-
+    yield interface.ERC20("0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e")
--- tests/test_subdao.py
+++ tests/test_subdao.py
@@ -3,38 +3,58 @@ from brownie import Wei, reverts
 import brownie
 
 
-
-def test_subdaos(web3,LiquidityGuageV4, SimpleVesting, Voter, chain, GovernanceToken, SubDaoFactory, gov, yfi_whale, yfi):
+def test_subdaos(
+    web3,
+    LiquidityGuageV4,
+    SimpleVesting,
+    Voter,
+    chain,
+    GovernanceToken,
+    SubDaoFactory,
+    gov,
+    yfi_whale,
+    yfi,
+):
 
     factory = gov.deploy(SubDaoFactory)
     ve_yfi = gov.deploy(LiquidityGuageV4, yfi, factory)
     factory.setVeYfi(ve_yfi)
 
-    yfi.approve(ve_yfi, 2**256-1, {'from': yfi_whale})
-    #lockup some yfi in the veYFI
-    ve_yfi.deposit(100*1e18, {'from': yfi_whale})
+    yfi.approve(ve_yfi, 2 ** 256 - 1, {"from": yfi_whale})
+    # lockup some yfi in the veYFI
+    ve_yfi.deposit(100 * 1e18, {"from": yfi_whale})
 
-    to_yfi = 50*1e18
-    to_locked_treasury = 50*1e18
-    to_open_treasury = 25*1e18
-    to_team = 25*1e18
+    to_yfi = 50 * 1e18
+    to_locked_treasury = 50 * 1e18
+    to_open_treasury = 25 * 1e18
+    to_team = 25 * 1e18
 
-    vestingLength = 1000 # seconds
+    vestingLength = 1000  # seconds
     cliffLength = 500
 
     share = [to_yfi, to_locked_treasury, to_open_treasury, to_team]
 
-    launchPhase = 10 # 10 blocks (real life will be more like 6 months)
-    votePeriod = 10 # 10 blocks (real life will be more like 7 days)
+    launchPhase = 10  # 10 blocks (real life will be more like 6 months)
+    votePeriod = 10  # 10 blocks (real life will be more like 7 days)
 
-   
-    tx = factory.newSubDao("poolpiDAO", "POO", gov, share, launchPhase, votePeriod, cliffLength, vestingLength)
-    
+    tx = factory.newSubDao(
+        "poolpiDAO",
+        "POO",
+        gov,
+        share,
+        launchPhase,
+        votePeriod,
+        cliffLength,
+        vestingLength,
+    )
+
     token = GovernanceToken.at(tx.events["NewSubDao"]["govToken"])
     voter = Voter.at(tx.events["NewSubDao"]["voter"])
     vester = SimpleVesting.at(tx.events["NewSubDao"]["teamVester"])
 
-    assert token.totalSupply() == to_yfi + to_locked_treasury + to_open_treasury + to_team
+    assert (
+        token.totalSupply() == to_yfi + to_locked_treasury + to_open_treasury + to_team
+    )
     assert token.balanceOf(voter) == to_locked_treasury
     assert token.balanceOf(ve_yfi) == to_yfi
     assert token.balanceOf(gov) == to_team
@@ -42,15 +62,24 @@ def test_subdaos(web3,LiquidityGuageV4, SimpleVesting, Voter, chain, GovernanceT
     chain.mine(1)
     assert ve_yfi.claimable_reward(yfi_whale, token) > 0
 
-    tx2 = factory.newSubDao("poolpiDAO2", "POO2", gov, share, launchPhase, votePeriod, cliffLength, vestingLength)
-    
+    tx2 = factory.newSubDao(
+        "poolpiDAO2",
+        "POO2",
+        gov,
+        share,
+        launchPhase,
+        votePeriod,
+        cliffLength,
+        vestingLength,
+    )
+
     token2 = GovernanceToken.at(tx.events["NewSubDao"]["govToken"])
     voter2 = Voter.at(tx.events["NewSubDao"]["voter"])
     chain.mine(1)
     assert ve_yfi.claimable_reward(yfi_whale, token2) > 0
-    token.delegate(yfi_whale, {'from': yfi_whale})
+    token.delegate(yfi_whale, {"from": yfi_whale})
 
-    tx = ve_yfi.claim_rewards({'from': yfi_whale})
+    tx = ve_yfi.claim_rewards({"from": yfi_whale})
     print(tx.events)
     assert token.balanceOf(yfi_whale) > 0
 
@@ -63,7 +92,7 @@ def test_subdaos(web3,LiquidityGuageV4, SimpleVesting, Voter, chain, GovernanceT
     print(token.getPriorVotes(yfi_whale, voter.voteStart()))
     assert voter.currentLeader() == yfi_whale
 
-    #vote hasnt ended yet
+    # vote hasnt ended yet
     with brownie.reverts():
         voter.endVote({"from": yfi_whale})
 
@@ -76,12 +105,9 @@ def test_subdaos(web3,LiquidityGuageV4, SimpleVesting, Voter, chain, GovernanceT
     chain.sleep(600)
     chain.mine(1)
     toWithdraw = vester.totalVested()
-    print(toWithdraw/1e18)
+    print(toWithdraw / 1e18)
     assert toWithdraw > 0
 
-    vester.claim(gov, {'from': gov} )
+    vester.claim(gov, {"from": gov})
     toWithdraw = vester.withdrawableAmount()
     assert toWithdraw == 0
-
-
-
