b9a668dbe331915394c1afcb14b2c11eb2d8ad33
flashfish <flashfish0x@protonmail.com>
2022-01-06 17:12:47 +0000

    voter first draft

--- contracts/SubDaoFactory.sol
+++ contracts/SubDaoFactory.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./GovernanceToken.sol";
+import "./Voter.sol";
 
 interface VeYFI {
     function add_reward(address reward, address distributor) external;
@@ -13,19 +16,63 @@ interface VeYFI {
 
 //create all the steps for a subdao
 contract SubDaoFactory is AccessControl{
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    address public tokenImplementation;
+    address public voterImplementation;
+    mapping(address => DaoInfo) daos;
 
-    VeYFI public veYfi;
+    // Info of each subDao.
+    struct DaoInfo {
+        address voter;
+        uint256 launchBlock;
+        uint256 voteStart; 
+        uint256 voteEnd;
+        address originalGov;
+    }
+
+    address public veYfi;
 
     constructor() public {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
-    function newSubDao(string memory name, string memory symbol, address initialMultisig, uint256 amount, uint256 toYfi, toLockedTreasury, toOpenTreasury, toTeam) external returns (address){
+    function newSubDao(string memory _name, string memory _symbol, address initialMultisig, uint256 toYfi, uint256 toLockedTreasury, uint256 toOpenTreasury, uint256 toTeam, uint256 launchPhase, uint256 votePeriod) external returns (address govToken){
+        require(veYfi != address(0), "veYFI not set");
 
-        GovernanceToken gt = new GovernanceToken(name, symbol);
-        uint256 total = toYfi.add(toTreasury).add(toOpenTreasury).add(toTeam);
+        uint256 totalSupply = toYfi.add(toLockedTreasury).add(toOpenTreasury).add(toTeam); 
 
+        //create governance token
+        GovernanceToken gt;
+
+        if(tokenImplementation == address(0)){
+            gt = new GovernanceToken();
+        }else{
+            gt = GovernanceToken(clone(tokenImplementation));
+            
+        }
+        gt.initialise(_name, _symbol, totalSupply);
+
+        Voter vt;
+        if(voterImplementation == address(0)){
+            vt = new Voter();
+        }else{
+            vt = Voter(clone(voterImplementation));
+        }
+        govToken = address(gt);
+        (uint256 launchBlock, uint256 voteStart, uint256 voteEnd) = vt.initialise(launchPhase, votePeriod, govToken);
+
+        
+        
+
+        daos[govToken] = DaoInfo({
+            voter: address(vt),
+            launchBlock: launchBlock,
+            voteStart: voteStart,
+            voteEnd: voteEnd,
+            originalGov: initialMultisig
+        });
         
     }
 
@@ -34,7 +81,21 @@ contract SubDaoFactory is AccessControl{
     function setVeYfi(address _veYfi) public{
         require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Admin Only");
 
-        veYfi = VeYFI(_veYfi);
+        veYfi = _veYfi;
+    }
+
+
+    //simple cloner for minimal proxy by openzeppelin
+    function clone(address master) internal returns (address instance) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+            mstore(add(ptr, 0x14), shl(0x60, master))
+            mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+            instance := create(0, ptr, 0x37)
+        }
+        require(instance != address(0), "ERC1167: create failed");
     }
 
 
